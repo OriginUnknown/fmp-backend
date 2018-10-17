f235dbb3273b2408ed870795eacd0eb4153dbd7e
OriginUnknown <ugoego@gmail.com>
2018-10-17 10:15:38 +0100

    [prime-numbers] - Helper functions added to run script in node cli

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 const isPrimeNumber = require("./prime-numbers");
 const tableGenerator = require("./table-generator");
+const fs = require("fs");
+
+const generatePrimeNumberTable = (num) => {
+    const output = tableGenerator(num, isPrimeNumber);
+    fs.writeFile('./output-file.txt', output, 'utf-8', (err) => {
+        if(err) {
+            throw err;
+        }
+        console.log('file saved');
+    });
+}
+
+const renderPrimeNumberTable = () => {
+    fs.readFile('./output-file.txt', 'utf-8', (err, data)=> {
+        if(err) {
+            throw err;
+        }
+        console.log(data);
+    });
+}
 
 module.exports = {
     isPrimeNumber,
-    tableGenerator
+    tableGenerator,
+    generatePrimeNumberTable,
+    renderPrimeNumberTable
 }
